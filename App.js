b40185f5f334846ca867ec5b089d6db7839e8265
Jalil <jalilsan@gmail.com>
2017-11-05 08:05:58 -0800

    Added Deck component

--- App.js
+++ App.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, ScrollView } from 'react-native'
+import Deck from './src/Deck'
+import { Card, Button } from 'react-native-elements'
 
-export default class App extends React.Component {
-  render() {
+const DATA = [
+  {
+    id: 1,
+    text: 'Card #1',
+    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg'
+  },
+  {
+    id: 2,
+    text: 'Card #2',
+    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
+  },
+  {
+    id: 3,
+    text: 'Card #3',
+    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
+  },
+  {
+    id: 4,
+    text: 'Card #4',
+    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
+  },
+  {
+    id: 5,
+    text: 'Card #5',
+    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg'
+  },
+  {
+    id: 6,
+    text: 'Card #6',
+    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
+  },
+  {
+    id: 7,
+    text: 'Card #7',
+    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
+  },
+  {
+    id: 8,
+    text: 'Card #8',
+    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg'
+  }
+]
+
+export default class App extends Component {
+  renderCard (item) {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+	<Card key={item.id}  title={item.text }  imageStyle={{width: 345}}  image={{uri: item.uri}}  >
+	 <Text style={{marginBottom: 10 }}>  You </Text>
+ 	<Button icon={{name: 'code' }} backgroundColor="#03A9F4" title="View Now" />
+	</Card>
+	);
+  }
+  render () {
+    return (
+      <ScrollView style={{flex: 1}} >
+        <Deck data={DATA} renderCard={this.renderCard} />
+      </ScrollView>
+    )
   }
 }
 
@@ -16,6 +69,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+
+ image: {
+  }
+})
+
